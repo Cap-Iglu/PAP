c9a4bce497510bb3f6edde3b72829e520847a92b
Cap-Iglu <luiscss24@gmail.com>
2019-05-14 17:14:24 +0100

    My Commit

--- src/main/java/com/example/pap_desta_vai/AcoresPdiact.java
+++ src/main/java/com/example/pap_desta_vai/AcoresPdiact.java
@@ -2,18 +2,9 @@ package com.example.pap_desta_vai;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.webkit.JavascriptInterface;
-import android.webkit.WebView;
 import android.widget.ImageButton;
-
-import static com.example.pap_desta_vai.R.menu.menu_main;
 
 public class AcoresPdiact extends AppCompatActivity {
 
@@ -38,7 +29,7 @@ public class AcoresPdiact extends AppCompatActivity {
         ldsc_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(AcoresPdiact.this, ldscact.class);
+                Intent intent = new Intent(AcoresPdiact.this, Bragamenuact.Bragancamenuact.Bragapdiact.castelobrancomenuact.cdbact.cndpact.contentmain.ldscact.class);
                 startActivity(intent);
             }
         });
--- src/main/java/com/example/pap_desta_vai/Bejamenuact.java
+++ src/main/java/com/example/pap_desta_vai/Bejamenuact.java
@@ -2,17 +2,9 @@ package com.example.pap_desta_vai;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.webkit.JavascriptInterface;
 import android.widget.ImageButton;
-
-import static com.example.pap_desta_vai.R.menu.menu_main;
 
 public class Bejamenuact extends AppCompatActivity {
 
@@ -30,7 +22,7 @@ public class Bejamenuact extends AppCompatActivity {
         voltarbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Bejamenuact.this, contentmain.class);
+                Intent intent = new Intent(Bejamenuact.this, Bragamenuact.Bragancamenuact.Bragapdiact.castelobrancomenuact.cdbact.cndpact.contentmain.class);
                 startActivity(intent);
             }
         });
