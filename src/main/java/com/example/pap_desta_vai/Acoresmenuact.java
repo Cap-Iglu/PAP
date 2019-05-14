d7748f4e4f02a07474145c7d53d76ad6be374495
Cap-Iglu <luiscss24@gmail.com>
2019-05-14 17:17:33 +0100

    My Commit

--- src/main/java/com/example/pap_desta_vai/Acoresmenuact.java
+++ src/main/java/com/example/pap_desta_vai/Acoresmenuact.java
@@ -22,7 +22,7 @@ public class Acoresmenuact extends AppCompatActivity {
         voltarbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Acoresmenuact.this, Bragamenuact.Bragancamenuact.Bragapdiact.castelobrancomenuact.cdbact.cndpact.contentmain.class);
+                Intent intent = new Intent(Acoresmenuact.this, MainActivity.class);
                 startActivity(intent);
             }
         });
