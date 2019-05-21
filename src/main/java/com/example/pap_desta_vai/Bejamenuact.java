7b11733dd58536b1eee4dd715a41d8c3181c593f
Cap-Iglu <luiscss24@gmail.com>
2019-05-21 16:47:03 +0100

    My Commit

--- src/main/java/com/example/pap_desta_vai/Bejamenuact.java
+++ src/main/java/com/example/pap_desta_vai/Bejamenuact.java
@@ -30,7 +30,7 @@ public class Bejamenuact extends AppCompatActivity {
         infbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Bejamenuact.this, AcoresInfact.class);
+                Intent intent = new Intent(Bejamenuact.this, Bejainfact.class);
                 startActivity(intent);
             }
         });
